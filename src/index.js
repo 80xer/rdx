5bcade601d45c7469647a6d8096b319967cc39c1
leetou <leetou@fnguide.com>
2018-10-16 20:24:21 +0900

    vanilla counter with redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-import style from './main.css';
-console.log(`I'm a silly entry point`);
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
+import { createStore } from 'redux';
+import './main.css';
+
+function counter(state, action) {
+  if (typeof state === 'undefined') {
+    return 0;
+  }
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+function render() {
+  document.getElementById('value').innerHTML = store.getState().toString();
+}
+
+render();
+
+store.subscribe(render);
+
+document.getElementById('increment').addEventListener('click', function() {
+  store.dispatch({ type: 'INCREMENT' });
+});
+
+document.getElementById('decrement').addEventListener('click', function() {
+  store.dispatch({ type: 'DECREMENT' });
+});

30d074aadc624cc2b3c785596b84768d9829efca
leetou <leetou@fnguide.com>
2018-10-16 20:04:56 +0900

    add html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -8,6 +10,21 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
     ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
